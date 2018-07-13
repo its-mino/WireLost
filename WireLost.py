8d582b79db40fcfe4dbbed791e85c67dd88bb19a
Brendan <ulxlx71@aim.com>
2018-07-12 20:37:22 -0400

    added threading to handle multiple connections

--- WireLost.py
+++ WireLost.py
@@ -4,6 +4,7 @@ import math
 import random
 import sys
 from argparse import ArgumentParser
+from threading import Thread
 
 parser = ArgumentParser()
 parser.add_argument("role", help="Choose whether to run as server or client")
@@ -22,37 +23,40 @@ def combine(lov):
 	    del lov[(len(lov)-1)-i]
 
 	average = float(sum(lov)/len(lov))
-
+	return average
 	#prints list, sum, length, and average
-	print(lov)
-	print(sum(lov))
-	print(len(lov))
-	print(average)
+	#print(lov)
+	#print(sum(lov))
+	#print(len(lov))
+	#print(average)
 
-
-def be_server():
-	HOST = ''   
-	PORT = 50007
-	times = []
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind((HOST, PORT))
-	s.listen(1)
-	conn, addr = s.accept()
+def send_data(i, addr, conn):
 	with open('zero', 'rb') as file:
+		times = []
 		data = file.read()
 		for i in range(50):
-			print(i)
 			start_time = datetime.datetime.now()
 			conn.sendall(data)
 			conn.recv(1)
 			end_time = datetime.datetime.now()
 			times.append((end_time-start_time).total_seconds())
 	conn.close()
-	combine(times)
+	avg = combine(times)
+	print(str(addr)+": "+str(avg))
 
-def be_client():
-	HOST = ''
+def be_server():
+	HOST = ''   
 	PORT = 50007
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((HOST, PORT))
+	i = 0
+	while True:
+		s.listen(5)
+		conn, addr = s.accept()
+		t = Thread(target=send_data, args=(i,addr,conn))
+		t.start()
+
+def recieve_data(i, HOST, PORT):
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.connect((HOST, PORT))
 	for i in range(50):
@@ -63,8 +67,14 @@ def be_client():
 		s.sendall(b'a')
 	s.close()
 
+def be_client(num):
+	HOSTS = ['','','','']
+	PORT = 50007
+	for i in range(num):
+		t = Thread(target=recieve_data, args=(i,HOSTS[i],PORT))
+		t.start()
 
 if args.role.lower() == 's':
 	be_server()
 else:
-	be_client()
+	be_client(int(args.role))
